ed1ab34ca30d0f4a250b1220b28a629b16118bd7
Hladiuc Ionel-Dorel <dorel.hladiuc@student.usv.ro>
2024-11-06 22:19:48 +0200

    Tema 4

--- App/App/Program.cs
+++ App/App/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using App.lab4;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,16 @@ using System.Threading.Tasks;
 
 namespace App
 {
+    /// <summary>
+    /// The main entry point for the application. This class initializes and runs the 3D rendering window.
+    /// </summary>
     internal class Program
     {
+        /// <summary>
+        /// The main method, which serves as the entry point for the application.
+        /// Initializes a new instance of <see cref="Window3D"/> and starts the rendering loop.
+        /// </summary>
+        /// <param name="args">Command-line arguments (not used in this application).</param>
         static void Main()
         {
             //using (Window window = new Window())
@@ -20,9 +29,14 @@ namespace App
             //    axis.Run(30.0, 0.0);
             //}
 
-            using (Triangle triangle = new Triangle())
+            //using (Triangle triangle = new Triangle())
+            //{
+            //    triangle.Run(30.0, 0.0);
+            //}
+
+            using (Window3D window = new Window3D()) 
             {
-                triangle.Run(30.0, 0.0);
+                window.Run(30.0, 0.0);
             }
         }
     }
